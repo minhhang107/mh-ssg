c90df3174ec3833c39ae074e01d492a33e27335d
Minh Hang Nguyen <hang00103@gmail.com>
2021-09-16 10:13:18 -0400

    fixed validateOutputFolder()

--- bin/utils/validateOutputFolder.js
+++ bin/utils/validateOutputFolder.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 
-//return
 const validateOutputFolder = (folder) => {
+  if (folder === "dist") {
+    if (fs.existsSync(folder)) {
+      fs.rmdirSync(folder, { recursive: true, force: true });
+    }
+    fs.mkdirSync(folder);
+    return true;
+  }
+
   try {
     if (fs.existsSync(folder)) {
-      if (folder === "dist") {
-        fs.rmdirSync(folder, { recursive: true, force: true });
-        fs.mkdirSync(folder);
-      }
       return true;
     }
+    return false;
   } catch (err) {
     console.log(err);
     return false;
